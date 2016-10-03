6381df51e4b0ff29124edd4261d8c926e1c4e1ae
William <williamnharvey@gmail.com>
2016-10-03 19:47:03 -0400

    'icons'

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var morgan = require('morgan');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
