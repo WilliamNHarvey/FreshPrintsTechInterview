2b9cdc871c66f68fe463164d2f39d5189c14a747
William <williamnharvey@gmail.com>
2016-10-04 18:40:10 -0400

    image

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.post('/upload', upload.single('avatar'), function (req, res) {
         
     if (path.extname(req.file.originalname).toLowerCase() === '.png' || path.extname(req.file.originalname).toLowerCase() === '.jpg' || path.extname(req.file.originalname).toLowerCase() === '.jpeg') {
     	var random = makeid()
-    	console.log('./public/user_img/' + random + path.extname(req.files.file.originalname).toLowerCase());
+    	console.log('./public/user_img/'.concat(random.toString()).concat(path.extname(req.files.file.originalname).toLowerCase().toString()));
     	targetPath = path.resolve('./public/user_img/' + random + path.extname(req.files.file.originalname).toLowerCase());
     	console.log(targetPath);
         fs.rename(tempPath, targetPath, function(err) {
