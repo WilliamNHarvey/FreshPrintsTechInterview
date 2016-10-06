f7beeb35c5d4f1f5d991417285de69d204a968d8
William <williamnharvey@gmail.com>
2016-10-06 00:59:12 -0400

    saves

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.post('/save', function (req, res, next) {
 			if(err) {
 				console.log(err);
 			} else {
-				console.log('select saves start');
+				console.log(result);
 			}
 		});
 	connection.query("SELECT * FROM user_saves WHERE ip='"+ipAdd+"'",
@@ -120,7 +120,6 @@ app.post('/save', function (req, res, next) {
 			if(err) {
 				console.log(err);
 			} else {
-				console.log(result[0].save2);
 				if(result.length == 0) {
 					connection.query('INSERT INTO user_saves SET ?', {ip: ipAdd, save1: insert, save2: null, save3: null, save4: null, save5: null, save6: null},
 							function(err, result){
