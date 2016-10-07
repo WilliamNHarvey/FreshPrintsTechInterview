d470e130ff0fea21a853af0d97f78ef59e4cf124
William <williamnharvey@gmail.com>
2016-10-07 05:14:09 -0400

    buttons

--- app.js
+++ app.js
@@ -14,14 +14,38 @@ var users = require('./routes/users');
 var app = express();
 var upload = multer({ dest: './public/user_img/' })
 app.use(morgan('combined'));
-var connection = mysql.createConnection({
-	host 	 : 'us-cdbr-iron-east-04.cleardb.net',
-	user 	 : 'bb1f84da0b6428',
-	password : 'f7cbf300',
-	database : 'heroku_59d4cdfdabbc8e9'
-})
+
+var db_config = {
+		host 	 : 'us-cdbr-iron-east-04.cleardb.net',
+		user 	 : 'bb1f84da0b6428',
+		password : 'f7cbf300',
+		database : 'heroku_59d4cdfdabbc8e9'
+};
 
 connection.connect();
+
+function handleDisconnect() {
+	  connection = mysql.createConnection(db_config); // Recreate the connection, since
+	                                                  // the old one cannot be reused.
+
+	  connection.connect(function(err) {              // The server is either down
+	    if(err) {                                     // or restarting (takes a while sometimes).
+	      console.log('error when connecting to db:', err);
+	      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+	    }                                     // to avoid a hot loop, and to allow our node script to
+	  });                                     // process asynchronous requests in the meantime.
+	                                          // If you're also serving http, display a 503 error.
+	  connection.on('error', function(err) {
+	    console.log('db error', err);
+	    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+	      handleDisconnect();                         // lost due to either server restart, or a
+	    } else {                                      // connnection idle timeout (the wait_timeout
+	      throw err;                                  // server variable configures this)
+	    }
+	  });
+	}
+
+handleDisconnect();
 /*connection.query('DROP TABLE user_saves',
 function(err, result){
 	// Case there is an error during the creation
